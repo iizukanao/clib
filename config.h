52b605701df59514a706f3a7655b5fc750860aa0
Nao Iizuka <iizuka@kyu-mu.net>
2013-06-25 03:24:31 +0900

    Add config.h

---
+++ config.h
@@ -0,0 +1,21 @@
+#ifndef _CLIB_CONFIG_H_
+#define _CLIB_CONFIG_H_
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#define WIDTH 480
+#define HEIGHT 360
+
+#define TARGET_FPS 30
+#define H264_BIT_RATE 500000
+
+#define AUDIO_SAMPLE_RATE 22050
+#define AAC_BIT_RATE 32000
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
