8764fa18de31dfc758cb03524ce839485eee1578
Nao Iizuka <iizuka@kyu-mu.net>
2013-06-27 02:19:52 +0900

    config.h: Add comments

--- config.h
+++ config.h
@@ -5,13 +5,20 @@
 extern "C" {
 #endif
 
+// Dimension of video image
 #define WIDTH 480
 #define HEIGHT 360
 
+// Video frame rate
 #define TARGET_FPS 30
+
+// Bit rate of H.264 video
 #define H264_BIT_RATE 500000
 
+// Sample rate of audio
 #define AUDIO_SAMPLE_RATE 22050
+
+// Bit rate of AAC audio
 #define AAC_BIT_RATE 32000
 
 #if defined(__cplusplus)
